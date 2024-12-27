9a5977cd076e2a6e5477ebcdacd3401f4b7229ec
2370823 <devanshujha8305@gmail.com>
2024-12-27 17:13:34 +0530

    Created A SpringBootRest api which returns a List

--- springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -1,5 +1,8 @@
 package net.javaguides.springboot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import net.javaguides.springboot.bean.Student;
@@ -11,5 +14,15 @@ public class StudentController {
 	public Student getStudent() {
 		Student student = new Student(1,"Ramesh","Fadetare");
 		return student;
+	}
+	
+	@GetMapping("students")
+	public List<Student> getStudents() {
+		List<Student> students = new ArrayList<>();
+		students.add(new Student(1,"Ramesh","Sharma"));
+		students.add(new Student(2, "Umesh", "Yadav"));
+		students.add(new Student(3, "Rohit", "Sharma"));
+		students.add(new Student(4, "Sanjaya", "Singhania"));
+		return students;
 	}
 }
