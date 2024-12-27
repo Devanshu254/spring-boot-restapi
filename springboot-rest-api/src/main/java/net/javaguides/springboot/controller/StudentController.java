50cfe0b6b7796176326ff4b018cb748bde42a4a5
2370823 <devanshujha8305@gmail.com>
2024-12-27 17:03:50 +0530

    This commit should be ignored

--- springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import net.javaguides.springboot.bean.Student;
 
-// Rest controller is a combination of @Controller and @Response body. Just that
+// Rest controller is a combination of @Controller and @Response body. Just that. 
 @RestController
 public class StudentController {
 	@GetMapping("student")
